68b1f691e54053ccf20d01b23b7ed1d91d590bd8
SelenaRosalesTello <138831152+SelenaRosalesTello@users.noreply.github.com>
2023-07-06 19:08:51 -0500

    mi primer commit

---
+++ app/src/main/java/com/selena/playzoom/MainActivity.java
@@ -0,0 +1,28 @@
+package com.selena.playzoom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.selena.playzoom.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    //private Button btnGetStarted;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding= ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.btnGetStarted.setOnClickListener(v->{
+            Toast.makeText(this,"Comenzar",Toast.LENGTH_SHORT).show();
+        });
+        binding.textMovie.setText("Mira tu pelicula\nfavorita.");
+
+    }
+}
